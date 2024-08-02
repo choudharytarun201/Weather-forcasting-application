70d5b6e3ae06f7aa73f0da8e0fe58d726e5844e9
Tarun choudhary <choudharytarun201@gmail.com>
2024-08-02 22:45:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { weather } from './components/weather';
+
+function App() {
+  return (
+    <div className="App">
+      <weather></weather>
+   
+    </div>
+  );
+}
+
+export default App;
